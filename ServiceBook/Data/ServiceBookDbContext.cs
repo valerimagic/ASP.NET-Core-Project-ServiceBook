3110447238c5b6d87aa71f94456c3ef1c405b455
Valeri Hristov <valerimagic@gmail.com>
2022-05-18 19:22:53 +0300

    update

---
+++ ServiceBook/Data/ServiceBookDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace ServiceBook.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ServiceBookDbContext : IdentityDbContext
+    {
+        public ServiceBookDbContext(DbContextOptions<ServiceBookDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
